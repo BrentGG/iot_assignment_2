a5f5e34508866a34101a18dd9a8233305e562baf
BrentGG <BrentGG@users.noreply.github.com>
2022-10-11 20:02:24 +0000

    terraform fmt

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "docker" {
-  host    = "npipe:////.//pipe//docker_engine"
+  host = "npipe:////.//pipe//docker_engine"
 }
 
 /* tried a bunch of things to pull the latest version but doesn't work
